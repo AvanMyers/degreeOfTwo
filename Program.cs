0a0b5d36e13a1e928a1c50cf21eebeb30ed9f934
AvanMyers <smail1341@gmail.com>
2024-09-09 22:06:19 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace degreeOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int strterNumber = 2;
+            int finalnumber = strterNumber;
+            int degree = 1;
+            int value = random.Next(2, 100);
+            bool isOpen = true;
+
+            Console.WriteLine("Случайное число " + value);
+
+            while (isOpen)
+            {
+                if (value < finalnumber)
+                {
+                    isOpen = false;
+                }
+                else
+                {
+                    finalnumber *= strterNumber;
+                    degree++;
+                }
+            }
+
+            Console.WriteLine($"Для числа {value} будет {strterNumber} в степени {degree}, то есть {finalnumber}. {value} < {finalnumber}");
+            Console.ReadKey();
+        }
+    }
+}
